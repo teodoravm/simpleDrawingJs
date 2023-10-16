4d214ff9f88a5e0c96eb8c30d8fc61843e75090a
Teodora Mindizova <tmindizova@gmail.com>
2023-10-16 15:49:56 +0300

    implemented addTiles function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // Containers - use those for interaction with them
 const container1 = document.getElementById('container-1');
 const container2 = document.getElementById('container-2');
+const tiles = [];
 
 // Setup initial tile
 const tile = createTileObject('Tile 1', 50, 60);
@@ -14,10 +15,17 @@ function createTileObject(name, top, left) {
   newTile.innerText = name;
   newTile.style.top = top + 'px';
   newTile.style.left = left + 'px';
+  tiles.push(name);
 
   return newTile;
 }
 
-function addTile() {}
+function addTile() {
+  const input = document.getElementById('tile-name').value;
+  const randomTop = Math.floor(Math.random() * (400 - 40));
+  const randomLeft = Math.floor(Math.random() * (500 - 120));
+  const tile1 = createTileObject(input, randomTop, randomLeft);
+  container1.appendChild(tile1);
+}
 
 function sortTiles() {}
