5e7cf23b9115f2ae8227792b1d076f5b3434de30
Teodora Mindizova <tmindizova@gmail.com>
2023-10-24 18:54:11 +0300

    small fixes for better readability

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 // Containers - use those for interaction with them
 const container1 = document.getElementById('container-1');
 const container2 = document.getElementById('container-2');
-const tileHeight = 40;
-const tileWidth = 120;
-const padding = 10;
-const tileHeightWithPadding = 50;
-const tileWidthWithPadding = 130;
-const containerHeight = 400;
-const containerWidth = 500;
+const TILE_HEIGHT = 40;
+const TILE_WIDTH = 120;
+const PADDING = 10;
+const TILE_HEIGHT_WITH_PADDING = 50;
+const TILE_WIDTH_WITH_PADDING = 130;
+const CONTAINER_INITIAL_HEIGHT = 400;
+const CONTAINER_WIDTH = 500;
 let idCounter = 0;
 
 // Setup initial tile
@@ -40,75 +40,82 @@ function createTileObject(name, top, left) {
     return newTile;
 }
 
+function generateRandomPosition(pos) {
+    if (pos === 'top') {
+        return Math.floor(
+            Math.random() * (CONTAINER_INITIAL_HEIGHT - TILE_HEIGHT)
+        );
+    } else {
+        return Math.floor(Math.random() * (CONTAINER_WIDTH - TILE_WIDTH));
+    }
+}
+
 function addTile() {
     const input = document.getElementById('tile-name').value;
 
     if (input !== '') {
-        const randomTop = Math.floor(
-            Math.random() * (containerHeight - tileHeight)
-        );
-        const randomLeft = Math.floor(
-            Math.random() * (containerWidth - tileWidth)
-        );
+        const randomTop = generateRandomPosition('top');
+        const randomLeft = generateRandomPosition('left');
         const tile1 = createTileObject(input, randomTop, randomLeft);
         container1.appendChild(tile1);
     }
 }
 
-function sortContainer(container, children) {
-    const tiles = Array.from(children);
+function sortContainer(container) {
+    const tiles = Array.from(container.childNodes);
     tiles.sort((a, b) => a.innerText.localeCompare(b.innerText));
 
-    let top = -40;
+    let top = -TILE_HEIGHT;
     let left = 0;
-    let heightToAdd = 10;
+    let heightToAdd = PADDING;
 
     //calculating the maximum count of tiles on a row inluding the padding before each one
     const maxElementsOnLine = parseInt(
-        container.offsetWidth / (tileWidth + padding * 3)
+        container.offsetWidth / (TILE_WIDTH + PADDING * 3)
     );
     //calculating the maximum count of tiles in the container inluding the top and bottom padding
     const maxElementsInCont = parseInt(
-        (containerHeight / (tileHeight + padding * 2)) * maxElementsOnLine
+        (CONTAINER_INITIAL_HEIGHT / (TILE_HEIGHT + PADDING * 2)) *
+            maxElementsOnLine
     );
 
     container.innerHTML = '';
 
     for (let i = 0; i < tiles.length; i++) {
         const tileStyle = tiles[i].style;
+        const tileFirstOnRow = i % maxElementsOnLine === 0;
 
         //when the tile is the last on the line increment top and set left so that the tiles are centered
-        if (i % maxElementsOnLine === 0) {
-            top = top + tileHeightWithPadding;
-            //calculating the space on the left needed for centering the tiles by
-            //extracting the total space taken by the tiles on the row and dividing that by 2
+        if (tileFirstOnRow) {
+            top = top + TILE_HEIGHT_WITH_PADDING;
+            //calculating the space on the left needed for centering the tiles by extracting the total space taken by the tiles on the row and dividing that by 2
             left =
                 (container.offsetWidth -
-                    maxElementsOnLine * tileWidthWithPadding) /
+                    maxElementsOnLine * TILE_WIDTH_WITH_PADDING) /
                 2;
 
             if (i >= maxElementsInCont) {
                 container.style.minHeight =
-                    containerHeight + heightToAdd + 'px';
+                    CONTAINER_INITIAL_HEIGHT + heightToAdd + 'px';
 
-                heightToAdd = heightToAdd + tileHeightWithPadding;
+                heightToAdd = heightToAdd + TILE_HEIGHT_WITH_PADDING;
             }
         } else {
-            left = left + tileWidth;
+            left = left + TILE_WIDTH;
         }
 
         tileStyle.top = top + 'px';
         tileStyle.left = left + 'px';
 
-        left = left + padding;
+        left = left + PADDING;
 
         container.appendChild(tiles[i]);
     }
 }
 
 function sortTiles() {
-    sortContainer(container1, container1.childNodes);
-    sortContainer(container2, container2.childNodes);
+    sortContainer(container1);
+    sortContainer(container2);
 }
 
 function moveTile(id, containerFrom, containerTo) {
